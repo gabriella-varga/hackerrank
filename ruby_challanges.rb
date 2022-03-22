4f240183424b1658369fe3440778ce823fe6f66b
Gabriella Varga <77884626+gabriella-varga@users.noreply.github.com>
2022-03-22 16:05:27 +0100

    Update ruby_challanges.rb

--- ruby_challanges.rb
+++ ruby_challanges.rb
@@ -6,3 +6,14 @@
 def process_text(arr)
     arr.map {|s| s.strip}.join(" ")
 end
+
+
+# Ruby - Enumerable - collect, ROT13
+# Why did the chicken cross the road?
+# Gb trg gb gur bgure fvqr!
+
+
+def rot13(secret_messages)
+    secret_messages.map { |c| c.tr("a-z", "n-za-m") }
+    # https://ruby-doc.org/core-3.1.1/String.html#method-i-tr
+end
